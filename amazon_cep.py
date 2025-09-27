4350c5c8b4ebf065cc04aa56fb504b4ba446412d
anticomm <93606441+anticomm@users.noreply.github.com>
2025-09-28 01:30:48 +0300

    Update amazon_cep.py

--- amazon_cep.py
+++ amazon_cep.py
@@ -2,6 +2,7 @@ import os
 import json
 import time
 import base64
+import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
