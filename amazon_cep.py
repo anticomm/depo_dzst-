f5a1394ef02e3e578fb68aec1b2feba339dcbe26
anticomm <93606441+anticomm@users.noreply.github.com>
2025-10-01 15:48:35 +0300

    Update amazon_cep.py

--- amazon_cep.py
+++ amazon_cep.py
@@ -50,7 +50,7 @@ def load_cookies(driver):
 
 def get_driver():
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
